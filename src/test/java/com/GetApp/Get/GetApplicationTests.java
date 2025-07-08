b163f4e734c14ada8ab35a1efae58df4146ddf0d
Naveen Dhakad <Naveendhakad489@gmail.com>
2025-07-08 23:40:31 +0530

    first commit

---
+++ src/test/java/com/GetApp/Get/GetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GetApp.Get;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
